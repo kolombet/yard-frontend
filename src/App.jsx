0f72c833325796e134e9b759f86c54e1d33e47cd
kolombet <kirill.kolombet@plarium.com>
2017-07-18 17:53:55 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ export default () =>
     <ScrollToTop>
       <div className="App">
         <Header />
+        <Header />
         <Redirect from="/" to="/complexes" />
         <Route exact path="/complexes/" component={List} />
         <Route path="/complexes/:slug" component={Show} />
